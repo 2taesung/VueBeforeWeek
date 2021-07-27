020ffcd1dee445cb3aa1086f380faca6947bc197
unknown <dlxotjde@gmail.com>
2021-07-27 21:42:33 +0900

    Add DataBinding2

---
+++ first-vue-app/src/views/DBL2.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <tr :key="i" v-for="(pruduct, i) in productList">
+      <td>{{ product.product_name }}</td>
+
+    </tr>
+  </div>
+</template>
+<script>
+import ApiMixin from ''
+export default {
+  data() {
+    return {
+      productLis: []
+    }
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    async getList() {
+      this.productList = await this.$api("API", "get");
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
